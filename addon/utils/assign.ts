0474e12ceb01ea9b47e782cdec0adc4e71583520
bekzod <bekzod@me.com>
2020-01-22 14:57:56 +0500

    add fallback for getOwnPropertyDescriptors

--- addon/utils/assign.ts
+++ addon/utils/assign.ts
@@ -1,5 +1,22 @@
+let getOwnPropertyDescriptors: (obj: { [x: string]: any }) => { [x: string]: PropertyDescriptor };
+
+if (Object.getOwnPropertyDescriptors !== undefined) {
+  getOwnPropertyDescriptors = Object.getOwnPropertyDescriptors;
+} else {
+  getOwnPropertyDescriptors = function(obj: object) {
+    let descriptors = {};
+
+    Object.keys(obj).forEach(key => {
+      descriptors[key] = Object.getOwnPropertyDescriptor(obj, key);
+    });
+
+    return descriptors;
+  };
+}
+
+
 export default function pureAssign(...objects: object[]): object {
   return objects.reduce((acc, obj) => {
-    return Object.defineProperties(acc, Object.getOwnPropertyDescriptors(obj));
+    return Object.defineProperties(acc, getOwnPropertyDescriptors(obj));
   }, {});
 }
